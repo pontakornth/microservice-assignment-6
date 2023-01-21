e862e43c7cc843908faea2821f77cea0fa6674ea
pontakornth <pontakorn61@gmail.com>
2023-01-21 12:07:35 +0700

    complete assignment

--- example-1/video-streaming/src/index.js
+++ example-1/video-streaming/src/index.js
@@ -1,55 +1,73 @@
 const express = require("express");
 const fs = require("fs");
+const http = require("http");
+
+function sendViewedMessage(videoPath) {
+  const postOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  const requestBody = {
+    videoPath: videoPath,
+  };
+
+  const req = http.request("http://history/viewed", postOptions);
+  req.write(JSON.stringify(requestBody));
+}
 
 //
 // Setup event handlers.
 //
 function setupHandlers(app) {
-    app.get("/video", (req, res) => { // Route for streaming video.
-        
-        const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
-        fs.stat(videoPath, (err, stats) => {
-            if (err) {
-                console.error("An error occurred ");
-                res.sendStatus(500);
-                return;
-            }
-    
-            res.writeHead(200, {
-                "Content-Length": stats.size,
-                "Content-Type": "video/mp4",
-            });
-    
-            fs.createReadStream(videoPath).pipe(res);
-        });
+  app.get("/video", (req, res) => {
+    // Route for streaming video.
+
+    const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
+    fs.stat(videoPath, (err, stats) => {
+      if (err) {
+        console.error("An error occurred ");
+        res.sendStatus(500);
+        return;
+      }
+
+      res.writeHead(200, {
+        "Content-Length": stats.size,
+        "Content-Type": "video/mp4",
+      });
+
+      fs.createReadStream(videoPath).pipe(res);
     });
+  });
 }
 
 //
 // Start the HTTP server.
 //
 function startHttpServer() {
-    return new Promise((resolve, reject) => { // Wrap in a promise so we can be notified when the server has started.
-        const app = express();
-        setupHandlers(app);
-        
-        const port = process.env.PORT && parseInt(process.env.PORT) || 3000;
-        app.listen(port, () => {
-            resolve();
-        });
+  return new Promise((resolve, reject) => {
+    // Wrap in a promise so we can be notified when the server has started.
+    const app = express();
+    setupHandlers(app);
+
+    const port = (process.env.PORT && parseInt(process.env.PORT)) || 3000;
+    app.listen(port, () => {
+      resolve();
     });
+  });
 }
 
 //
 // Application entry point.
 //
 function main() {
-    return startHttpServer();
+  return startHttpServer();
 }
 
 main()
-    .then(() => console.log("Microservice online."))
-    .catch(err => {
-        console.error("Microservice failed to start.");
-        console.error(err && err.stack || err);
-    });
+  .then(() => console.log("Microservice online."))
+  .catch((err) => {
+    console.error("Microservice failed to start.");
+    console.error((err && err.stack) || err);
+  });
